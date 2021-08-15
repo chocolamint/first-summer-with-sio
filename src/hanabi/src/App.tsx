f150be2e2c01526df289eb11f8ce9ce8c018f8cf
しゃるしぃ <info@angelbell.jp>
2021-08-15 14:52:36 +0900

    ランダムなセリフを表示するようにした

--- src/hanabi/src/App.tsx
+++ src/hanabi/src/App.tsx
@@ -3,12 +3,28 @@ import 'SioBalloon';
 import SioBalloon from 'SioBalloon';
 
 function App() {
+
+  const sioDialogues = [
+    '綺麗なのな～(Ｕ///\'ᴗ\'///Ｕ)',
+    'はわはわ₍₍ ◝(Ｕ^ᴗ^Ｕ)◞ ₎₎',
+    'わたあめ食べたいなの～♡'
+  ];
+
+  const dialogue = pickRandom(sioDialogues);
+
   return (
     <div className="App">
       <canvas></canvas>
-      <SioBalloon dialogue="こんにちはせかい₍₍ ◝(Ｕ^ᴗ^Ｕ)◞ ₎₎" />
+      <SioBalloon dialogue={dialogue} />
     </div>
   );
+}
+
+function pickRandom<T>(arr: T[]) {
+  const min = 0;
+  const max = Math.floor(arr.length);
+  const index = Math.floor(Math.random() * (max - min) + min);
+  return arr[index];
 }
 
 export default App;
