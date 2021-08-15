282e69d6b1fc1097b7c54303b14a487a08cc62f3
しゃるしぃ <info@angelbell.jp>
2021-08-15 18:34:17 +0900

    SpecialThanks のスタイルを調整

--- src/hanabi/src/SpecialThanks.tsx
+++ src/hanabi/src/SpecialThanks.tsx
@@ -7,11 +7,15 @@ export default function SpecialThanks() {
 
     return visible ?
         (<div className="SpecialThanks">
-            <h1>しおとはじめてのはなびたいかい</h1>
-            <button className="HideSpecialThanksButton" onClick={toggleVisible} />
+            <h1>♡しおとはじめてのはなびたいかい♡</h1>
+            <h2>Special Thanks (Ｕ´ ' ᴗ &lt;`Ｕ)ミ☆</h2>
             <ul>
-                <li><a href="https://qiita.com/iNaoki04/items/5d420440cf3d89f54f82">JavaScriptで花火を作ってみよう！</a></li>
+                <li><a href="https://ja.reactjs.org/" target="_blank">React</a></li>
+                <li><a href="https://qiita.com/iNaoki04/items/5d420440cf3d89f54f82" target="_blank">JavaScriptで花火を作ってみよう！</a></li>
             </ul>
+            <div className="CloseButton">
+                <button className="HideSpecialThanksButton" onClick={toggleVisible}>とじる</button>
+            </div>
         </div>) :
         (<button className="ShowSpecialThanksButton" onClick={toggleVisible} />);
 
