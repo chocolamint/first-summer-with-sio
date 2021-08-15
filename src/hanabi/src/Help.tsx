117d9ecd19777dd475e8802c093a82a01d1bb8aa
しゃるしぃ <info@angelbell.jp>
2021-08-15 19:15:18 +0900

    ふい字を提供してくれている神様へ感謝のリンクを貼った

--- src/hanabi/src/Help.tsx
+++ src/hanabi/src/Help.tsx
@@ -15,6 +15,8 @@ export default function Help() {
             <ul>
                 <li><a href="https://ja.reactjs.org/" target="_blank">React</a></li>
                 <li><a href="https://qiita.com/iNaoki04/items/5d420440cf3d89f54f82" target="_blank">JavaScriptで花火を作ってみよう！</a></li>
+                <li><a href="https://hp.vector.co.jp/authors/VA039499/#hui" target="_blank">ふい字</a></li>
+                <li><a href="http://customtemplate.blog112.fc2.com/blog-entry-143.html" target="_blank">日本語フリーフォントの「ふい字」をWEBフォントで活用しよう！</a></li>
             </ul>
             <div className="CloseButton">
                 <button className="HideSpecialThanksButton" onClick={toggleVisible}>とじる</button>
