446fa387b387eb9b32c23517831d8f075dce1b33
しゃるしぃ <info@angelbell.jp>
2021-08-16 23:21:20 +0900

    とりあえずヘルプボタンと汐りんの吹き出しとトークボタンの配置を flex でそれなりにした

--- src/hanabi/src/SioBalloon.tsx
+++ src/hanabi/src/SioBalloon.tsx
@@ -2,7 +2,9 @@ import 'SioBalloon.css';
 
 function SioBalloon(props: { message: string, isVisible: boolean }) {
     const className = ['SioBalloon', props.isVisible ? 'show' : 'hide'].join(' ');
-    return <div className={className} dangerouslySetInnerHTML={{ __html: props.message }}></div>;
+    return <div className={className}>
+        <span dangerouslySetInnerHTML={{ __html: props.message }}></span>
+    </div>;
 }
 
 export default SioBalloon;
