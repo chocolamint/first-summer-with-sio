bd4260e39ff7c5287eddc6992a9c26766bac3c59
しゃるしぃ <info@angelbell.jp>
2021-08-16 09:41:18 +0900

    汐りんが返事を返すようにしたけど挙動がおかしい

---
+++ src/hanabi/src/SioResponse.ts
@@ -0,0 +1,38 @@
+const sioResponses = {
+    'こんばんはわ～': [
+        'こんばんはわの～<span class="emoticon">(Ｕ⌒▽⌒Ｕ三Ｕ⌒▽⌒Ｕ)(Ｕ⌒▽⌒Ｕ)</span>',
+        'はわの<span class="emoticon">(Ｕ \'ᴗ\' Ｕ)</span>',
+        'こんばんはわ～<span class="emoticon">(Ｕ∩ \'ᴗ\' ∩Ｕ)</span>',
+    ] as const,
+    '今日もかわいいね': [
+        '本当に～？嬉しまるな<span class="emoticon">(Ｕ ∩\'ᴗ\'///Ｕ)</span>♡',
+        'えへへ…かわいかろう？<span class="emoticon">(Ｕ⌒▽⌒Ｕ)</span>',
+        'しおは～絶対アイドルなのからかわいいに決まってるってわけ<span class="emoticon">(Ｕ´ \' ᴗ <`Ｕ)ミ☆</span>',
+    ] as const,
+    '浴衣似合ってるよ': [
+        'わーい、嬉しまる～<span class="emoticon">₍₍ ◝(Ｕ^ᴗ^Ｕ)◞ ₎₎</span>',
+        '浴衣姿のしおと夜空の花火、どっちがかわいまる？<span class="emoticon">(Ｕ ˘ᴗ˘ Ｕ)</span>'
+    ] as const,
+    '花火綺麗だね': [
+        '本当に綺麗なのな～<span class="emoticon">(Ｕ∩ \'ᴗ\' ∩Ｕ)</span>',
+        'しおね、打ち上げ花火観るの生まれて初めてなの♡',
+        '花火にばかり見惚れてないで、しおのこともちゃんと見ての？<span class="emoticon">（ \'ᴗ\'ＵΞＵ\'ᴗ\'）</span>'
+    ] as const,
+    '夏祭り楽しかったね': [
+        '楽しかったなの～<span class="emoticon">(Ｕ⌒▽⌒Ｕ三Ｕ⌒▽⌒Ｕ)',
+        '花火大会が終わったあとも、もう少し見て回りたいなの<span class="emoticon">(Ｕ＞△＜Ｕ)</span>',
+        'きみと一緒だったから、なおさら楽しかったなのな<span class="emoticon">(Ｕ ∩\'ᴗ\'///Ｕ)</span>'
+    ] as const,
+    '一緒にいられて幸せ': [
+        'しおもそう思ってまるよ<span class="emoticon">(Ｕ ∩\'ᴗ\'///Ｕ)</span>♡',
+        '今日は一緒に来られてよかったなのな<span class="emoticon">₍₍ ◝(Ｕ^ᴗ^Ｕ)◞ ₎₎</span>'
+    ] as const,
+    '来年もまた来ようね': [
+        'もちろん！約束なの<span class="emoticon">(Ｕ \'ᴗ\'  )⊃━━*.☆ﾟ</span>'
+    ] as const,
+    '楽しいね': [
+        'きみと一緒だから、なおさら楽しいなのな<span class="emoticon">(Ｕ ∩\'ᴗ\'///Ｕ)</span>'
+    ] as const,
+};
+
+export default sioResponses;
--- src/hanabi/src/useSioMessage.ts
+++ src/hanabi/src/useSioMessage.ts
@@ -2,29 +2,30 @@ import familyMessages from "FamilyMessages";
 import { randomNumber, randomInt } from "Random";
 import { useEffect, useState } from "react";
 import sioMessages from "SIoMessages";
+import sioResponses from "SioResponse";
 
 type SioMessages = typeof sioMessages[number] | '';
 type FamilyMessage = typeof familyMessages[number];
+type SioResponses = typeof sioResponses[FamilyMessage][number];
 
 export default function useSioMessage(option: { supress: boolean, request: FamilyMessage | null }) {
 
-    const [message, setMessage] = useState({ message: '' as SioMessages, index: -1, isTarking: false });
+    const [message, setMessage] = useState({ message: '' as SioMessages | SioResponses, isTarking: false });
 
     useEffect(() => {
 
-        const nextBalloonTimeout = randomNumber(6000, 15000);
+        const nextBalloonTimeout = option.request ? 400 : randomNumber(6000, 15000);
+
+        if (option.supress) return;
 
         const start = setTimeout(() => {
-            setMessage(prev => {
-                // 前回と同じ値は返さないようにする（連続で同じセリフが出るのを防ぐ）
-                let nextIndex: number;
-                do {
-                    nextIndex = randomInt(sioMessages.length);
-                } while (nextIndex === prev.index);
-                return { message: sioMessages[nextIndex], index: nextIndex, isTarking: true && !option.supress };
+            setMessage(() => {
+                const messages = option.request ? sioResponses[option.request] : sioMessages;
+                const nextIndex = randomInt(messages.length);
+                return { message: messages[nextIndex], isTarking: true && !option.supress };
             });
             const end = setTimeout(() => {
-                setMessage(prev => ({ message: prev.message, index: prev.index, isTarking: false }));
+                setMessage(prev => ({ message: prev.message, isTarking: false }));
                 clearTimeout(end);
             }, 3000);
         }, nextBalloonTimeout);
