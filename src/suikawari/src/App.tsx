a46a6353b9401f80594b0a61aceb69577db96971
しゃるしぃ <info@angelbell.jp>
2021-08-18 18:12:07 +0900

    しゅがりんが上から下にアニメーションするようにした

--- src/suikawari/src/App.tsx
+++ src/suikawari/src/App.tsx
@@ -1,15 +1,28 @@
 import 'App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Shugarin from 'Shugarin';
+import { setInterval, clearInterval } from 'timers';
 
 function App() {
 
   const [shugarinX, setShugarinX] = useState(document.body.clientWidth / 2);
-  const [shugarinY, setShugarinY] = useState('5vh');
+  const [shugarinY, setShugarinY] = useState(5);
+
+  useEffect(() => {
+    const shugarinAnimation = setInterval(() => {
+      if (shugarinY > 70) {
+        clearInterval(shugarinAnimation);
+      } else {
+        setShugarinY(prev => prev + 0.4);
+      }
+
+    }, 100);
+    return () => clearInterval(shugarinAnimation);
+  }, [shugarinY]);
 
   return (
     <div className="App">
-      <Shugarin x={shugarinX} y={shugarinY} />
+      <Shugarin x={shugarinX} y={shugarinY + 'vh'} />
     </div>
   );
 }
--- src/suikawari/src/Shugarin.tsx
+++ src/suikawari/src/Shugarin.tsx
@@ -1,5 +1,6 @@
 import shugarin from 'shugarin.png';
 import 'Shugarin.css';
+import { useEffect } from 'react';
 
 export default function Shugarin(props: { x: number, y: string }) {
     const shugarinStyle = {
