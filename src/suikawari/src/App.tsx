4319f2cea2238feba1e8736f48b6f43b2faa28ed
しゃるしぃ <info@angelbell.jp>
2021-08-18 17:49:30 +0900

    create-react-app のテンプレートを削除

--- src/suikawari/src/App.tsx
+++ src/suikawari/src/App.tsx
@@ -4,20 +4,7 @@ import Shugarin from 'Shugarin';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Shugarin />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Shugarin />
     </div>
   );
 }
