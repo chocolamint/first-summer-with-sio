24f98d7522eca7e47725ed899444380b8d1fc50e
しゃるしぃ <info@angelbell.jp>
2021-08-18 17:59:54 +0900

    しゅがりんを画面中央上部に表示

--- src/suikawari/src/App.tsx
+++ src/suikawari/src/App.tsx
@@ -1,10 +1,15 @@
 import 'App.css';
+import { useState } from 'react';
 import Shugarin from 'Shugarin';
 
 function App() {
+
+  const [shugarinX, setShugarinX] = useState(document.body.clientWidth / 2);
+  const [shugarinY, setShugarinY] = useState('5vh');
+
   return (
     <div className="App">
-      <Shugarin />
+      <Shugarin x={shugarinX} y={shugarinY} />
     </div>
   );
 }
