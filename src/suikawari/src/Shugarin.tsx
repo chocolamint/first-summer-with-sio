24224005c450fd3bc6752f372985ca15cb4a8a3a
しゃるしぃ <info@angelbell.jp>
2021-08-18 18:19:45 +0900

    しゅがりんのサイズを端末画面サイズに応じたものにした

--- src/suikawari/src/Shugarin.tsx
+++ src/suikawari/src/Shugarin.tsx
@@ -1,6 +1,5 @@
 import shugarin from 'shugarin.png';
 import 'Shugarin.css';
-import { useEffect } from 'react';
 
 export default function Shugarin(props: { x: number, y: string }) {
     const shugarinStyle = {
