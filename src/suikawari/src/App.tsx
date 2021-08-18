ee07a15b8ea9ee0a0ae129cfac8e8fb165ad306f
しゃるしぃ <info@angelbell.jp>
2021-08-18 22:32:14 +0900

    しゅがりんがすいかの位置まで辿り着いたときにすいかを割れたかどうかを判定した

--- src/suikawari/src/App.tsx
+++ src/suikawari/src/App.tsx
@@ -10,17 +10,24 @@ type ShugarinDirection = 'Straight' | 'MoreLeft' | 'MoreRight';
 
 function App() {
 
+  const shugarinImageId = 'Shugarin';
+  const watermelonImageId = 'Watermelon';
   const [watermelonX,] = useState(randomNumber(10, 90));
   const [shugarinX, setShugarinX] = useState(randomNumber(10, 90));
   const [shugarinY, setShugarinY] = useState(5);
   const [shugarinDirection, setShugarinDirection] = useState('Straight' as ShugarinDirection);
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     const moveDistances = [0.1, 0.2, 0.3, 0.5, 1.0, 2.0, 4.0] as const;
     const shugarinAnimation = setInterval(() => {
+      // すいか位置まで到着したとき
       if (shugarinY > 50) {
         clearInterval(shugarinAnimation);
-      } else {
+        alert(success ? '成功' : '失敗');
+      }
+      // 移動中
+      else {
         setShugarinY(prev => prev + 0.4);
         const moveDistance = moveDistances[randomInt(moveDistances.length)];
         switch (shugarinDirection) {
@@ -35,6 +42,16 @@ function App() {
         }
       }
 
+      const shugarinRect = document.getElementById('Shugarin')!.getBoundingClientRect();
+      const shugarinLeft = shugarinRect.left;
+      const shugarinWidth = shugarinRect.width;
+      const shugarinCenter = shugarinLeft + shugarinWidth / 2;
+      const watermelonRect = document.getElementById('Watermelon')!.getBoundingClientRect();
+      const watermelonLeft = watermelonRect.left;
+      const watermelonWidth = watermelonRect.width;
+      const watermelonRight = watermelonLeft + watermelonWidth;
+      setSuccess(watermelonLeft <= shugarinCenter && shugarinCenter <= watermelonRight);
+
     }, 100);
     return () => clearInterval(shugarinAnimation);
   }, [shugarinY, shugarinDirection]);
@@ -42,8 +59,8 @@ function App() {
   return (
     <div className="App">
       <main>
-        <Shugarin x={shugarinX} y={shugarinY} />
-        <Watermelon x={watermelonX} y={57} />
+        <Shugarin imageId={shugarinImageId} x={shugarinX} y={shugarinY} />
+        <Watermelon imageId={watermelonImageId} x={watermelonX} y={57} />
       </main>
       <footer>
         <AdvicePanel
--- src/suikawari/src/Shugarin.tsx
+++ src/suikawari/src/Shugarin.tsx
@@ -1,12 +1,12 @@
 import shugarin from 'shugarin.png';
 import 'Shugarin.css';
 
-export default function Shugarin(props: { x: number, y: number }) {
+export default function Shugarin(props: { imageId: string, x: number, y: number }) {
     const shugarinStyle = {
         left: props.x + 'vw',
         top: props.y + 'vh'
     };
     return <div className="Shugarin" style={shugarinStyle}>
-        <img src={shugarin} alt="しゅがりん" />
+        <img id={props.imageId} src={shugarin} alt="しゅがりん" />
     </div>;
 }
