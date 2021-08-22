cec6fa7756f1f113c236515045260842b9cbd5b7
Junya Wada <j.wada@sansan.com>
2021-08-22 14:36:50 +0900

    スタート画面でスタートを押したらゲームが始まるようにした

---
+++ src/suikawari/src/StartScreen.tsx
@@ -0,0 +1,5 @@
+export default function StartScreen(props: { onStart: () => void} ) {
+    return <div className="StartScreen">
+        <button onClick={props.onStart}>はじめる</button>
+    </div>;
+}
