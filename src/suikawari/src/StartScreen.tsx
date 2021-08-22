fe493bc1020a2b53b0940ecc0455e5900e96c525
Junya Wada <j.wada@sansan.com>
2021-08-22 14:44:07 +0900

    「スタート」ボタンの位置を調整

--- src/suikawari/src/StartScreen.tsx
+++ src/suikawari/src/StartScreen.tsx
@@ -1,3 +1,5 @@
+import 'StartScreen.css';
+
 export default function StartScreen(props: { onStart: () => void} ) {
     return <div className="StartScreen">
         <button onClick={props.onStart}>はじめる</button>
